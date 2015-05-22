a319cac3f004ee52447a0b1eefd76655d30b69e8
Andrii Vasyliev <sol@hiqdev.com>
2015-05-22 20:48:20 +0000

    + actual asset file

---
+++ FlagIconCssAsset.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @link      http://hiqdev.com/yii2-asset-flag-icon-css
+ * @license   BSD 3-clause
+ * @copyright Copyright (c) 2015 HiQDev
+ */
+
+namespace hiqdev\assets\FlagIconCss;
+
+class FlagIconCssAsset extends \yii\web\AssetBundle
+{
+    /**
+     * @inheritdoc
+     */
+    public $sourcePath = '@bower/flag-icon-css';
+
+    /**
+     * @inheritdoc
+     */
+    public $css = [
+        'css/flag-icon.min.css',
+    ];
+
+}
