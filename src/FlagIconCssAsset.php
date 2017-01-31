c5aa6477a494ce22b1bd6c16d7919b59e7658546
Andrii Vasyliev <sol@hiqdev.com>
2017-01-31 10:13:09 +0000

    csfixed

--- src/FlagIconCssAsset.php
+++ src/FlagIconCssAsset.php
@@ -1,12 +1,11 @@
 <?php
-
-/*
+/**
  * Yii2 asset for bower flag-icon-css
  *
  * @link      https://github.com/hiqdev/yii2-asset-flag-icon-css
  * @package   yii2-asset-flag-icon-css
  * @license   BSD-3-Clause
- * @copyright Copyright (c) 2015-2016, HiQDev (http://hiqdev.com/)
+ * @copyright Copyright (c) 2015-2017, HiQDev (http://hiqdev.com/)
  */
 
 namespace hiqdev\assets\flagiconcss;
